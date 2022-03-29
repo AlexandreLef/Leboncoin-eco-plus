8c6d517b799edc391dddc96d968f04144f3484f3
AlexandreLef <alex.lfebvre@gmail.com>
2022-03-29 13:59:31 +0200

    correction name to lastname

--- leboncoin-eco-plus/src/Form/UserType.php
+++ leboncoin-eco-plus/src/Form/UserType.php
@@ -16,7 +16,7 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('lastname', TextType::class)
             ->add('firstname', TextType::class)
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
